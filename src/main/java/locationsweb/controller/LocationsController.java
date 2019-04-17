9f6869e15df931b412f221389b9cde3d034ddb61
Siklér Gábor <sikler.gabor@innoteq.hu>
2019-04-17 13:37:43 +0200

    Thymeleaf view

--- src/main/java/locationsweb/controller/LocationsController.java
+++ src/main/java/locationsweb/controller/LocationsController.java
@@ -3,13 +3,19 @@ package locationsweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.View;
 
 @Controller
 public class LocationsController {
 
 	@RequestMapping("/locations")
 	@ResponseBody
-	public String locations() {
+	public String weblocations() {
 		return "<html><body><h1>Locations</h1></body></html>";
+	}
+	
+	@RequestMapping("/")
+	public String locations() {
+		return "index";
 	}
 }
