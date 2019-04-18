90335d1814cad97a4fdb7329edd34771ff74ca3a
Siklér Gábor <sikler.gabor@innoteq.hu>
2019-04-18 10:37:53 +0200

    Erőforrások kezelése

--- src/main/java/locationsweb/controller/LocationsController.java
+++ src/main/java/locationsweb/controller/LocationsController.java
@@ -1,9 +1,19 @@
 package locationsweb.controller;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
+
+import locationsweb.domain.Location;
 
 @Controller
 public class LocationsController {
@@ -13,9 +23,31 @@ public class LocationsController {
 	public String weblocations() {
 		return "<html><body><h1>Locations</h1></body></html>";
 	}
-	
+
 	@RequestMapping("/")
-	public String locations() {
-		return "index";
+	public ModelAndView locations() {
+
+		List<Object> locationsObjects = new ArrayList<>();
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime actualTime = LocalDateTime.now();
+
+		String header = "Locations";
+
+		Location loc1 = new Location((long) 1,"Pécs", 1, 1);
+		Location loc2 = new Location((long) 2,"Csorna", 2, 2);
+		Location loc3 = new Location((long) 3,"Tomaj", 3, 3);
+		
+		List<Location> locations = new ArrayList<>();
+		locations.add(loc1);
+		locations.add(loc2);
+		locations.add(loc3);
+		
+
+		locationsObjects.add(actualTime);
+		locationsObjects.add(header);
+		locationsObjects.add(locations);
+
+		return new ModelAndView("index", "locationsObjects", locationsObjects);
 	}
 }
---
+++ src/main/java/locationsweb/domain/Location.java
@@ -0,0 +1,113 @@
+package locationsweb.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double lat;
+	private double lon;
+
+	public Location(Long id, String name, double lat, double lon) {
+		this.id = id;
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+	}
+
+	public Location(String name, double lat, double lon) {
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+	}
+
+	public Location() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", name=" + name + ", lat=" + lat + ", lon=" + lon + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(lat);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(lon);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (Double.doubleToLongBits(lat) != Double.doubleToLongBits(other.lat))
+			return false;
+		if (Double.doubleToLongBits(lon) != Double.doubleToLongBits(other.lon))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
